40d6179d1a0729ba24966052896a55dae0bbe117
Troels Lenda <troelslenda@gmail.com>
2011-12-15 15:56:59 +0100

    creating simple builds of menus

--- theme/setup.js
+++ theme/setup.js
@@ -1,3 +1,31 @@
 jQuery(function(){
+  console.log(data);
+
+
+  // append list
+
+
+
+  // build floors list
+
+  $(data).each(function(i){
+    $('.plans').append('<li>'+this.name+'</li>');
+  });
+
+
+
+  $('.plans li').click(function(){
+    setKeywords($('.plans li').index(this));
+
+  });
+
+
+  function setKeywords(index){
+    $('.keywords').html('');
+    $(data[index].keywords).each(function(i){
+      console.log(this);
+      $('.keywords').append('<li>'+this.name+'</li>');
+    });
+  }
 
 });
