4a0b330c532d7e594ecc0e1ffa4bb0d18ca9d97d
Troels Lenda <troelslenda@gmail.com>
2011-12-15 15:56:45 +0100

    Defining datastructure

---
+++ theme/data.js
@@ -0,0 +1,138 @@
+/**
+ * @file
+ * This data contains the data that should be provided by the service
+ **/
+
+var data = [
+  {
+    'name' : 'Stuen',
+    'filename' : 'p0.gif',
+    'keywords' : [
+                  {
+                    'name' : 'Café',
+                    'id' : 0,
+                    'filname' : 'e0.gif'
+                  },
+                  {
+                    'name' : 'Receptionen',
+                    'id' : 1,
+                    'filname' : 'e0.gif'
+                  },
+                  {
+                    'name' : 'Borgerservice',
+                    'id' : 2,
+                    'filname' : 'e0.gif'
+                  },
+                  {
+                    'name' : 'Salonen',
+                    'id' : 3,
+                    'filname' : 'e0.gif'
+                  },
+                  {
+                    'name' : 'Intimscene',
+                    'id' : 4,
+                    'filname' : 'e0.gif'
+                  },
+                  {
+                    'name' : 'Toiletter',
+                    'id' : 5,
+                    'filname' : 'e0.gif'
+                  },
+                  {
+                    'name' : 'Elevator',
+                    'id' : 6,
+                    'filname' : 'e0.gif'
+                  },
+]
+  },
+  {
+    'name' : '1. sal',
+    'filename' : 'p0.gif',
+    'keywords' : [
+                  {
+                    'name' : 'Café',
+                    'id' : 7,
+                    'filname' : 'e0.gif'
+                  },
+                  {
+                    'name' : 'Receptionen',
+                    'id' : 8,
+                    'filname' : 'e0.gif'
+                  },
+                  {
+                    'name' : 'Toiletter',
+                    'id' : 5,
+                    'filname' : 'e0.gif'
+                  },
+                  {
+                    'name' : 'Elevator',
+                    'id' : 6,
+                    'filname' : 'e0.gif'
+                  },
+]
+  },
+  {
+    'name' : '2. sal',
+    'filename' : 'p0.gif',
+    'keywords' : [
+                  {
+                    'name' : 'Toiletter',
+                    'id' : 5,
+                    'filname' : 'e0.gif'
+                  },
+                  {
+                    'name' : 'Elevator',
+                    'id' : 6,
+                    'filname' : 'e0.gif'
+                  },
+                  {
+                    'name' : 'Borgerservice',
+                    'id' : 9,
+                    'filname' : 'e0.gif'
+                  },
+]
+  }
+  , {
+    'name' : '3. sal',
+    'filename' : 'p0.gif',
+    'keywords' : [
+                  {
+                    'name' : 'Café',
+                    'id' : 10,
+                    'filname' : 'e0.gif'
+                  },
+                  {
+                    'name' : 'Receptionen',
+                    'id' : 11,
+                    'filname' : 'e0.gif'
+                  },
+                  {
+                    'name' : 'Borgerservice',
+                    'id' : 4,
+                    'filname' : 'e0.gif'
+                  },
+                  {
+                    'name' : 'Salonen',
+                    'id' : 3,
+                    'filname' : 'e0.gif'
+                  },
+                  ]
+  }
+  , {
+    'name' : '4. sal',
+    'filename' : 'p0.gif',
+    'keywords' : [
+                  {
+                    'name' : 'Café',
+                    'id' : 6,
+                    'filname' : 'e0.gif'
+                  },
+                  {
+                    'name' : 'Receptionen',
+                    'id' : 12,
+                    'filname' : 'e0.gif'
+                  },
+  ]
+  }
+
+];
