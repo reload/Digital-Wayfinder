5a1509b0ce1ff4989f202f398ba65ecb35a4a217
Troels Lenda <troelslenda@gmail.com>
2011-12-22 15:33:53 +0100

    new images in data

--- theme/data.js
+++ theme/data.js
@@ -6,131 +6,131 @@
 var data = [
   {
     'name' : 'Stuen',
-    'filename' : 'p0.gif',
+    'filename' : 'p0.svg',
     'keywords' : [
                   {
                     'name' : 'Café',
                     'id' : 0,
-                    'filname' : 'e0.gif'
+                    'filename' : 'p0e1.svg'
                   },
                   {
                     'name' : 'Receptionen',
                     'id' : 1,
-                    'filname' : 'e0.gif'
+                    'filename' : 'p0e2.svg'
                   },
                   {
                     'name' : 'Borgerservice',
                     'id' : 2,
-                    'filname' : 'e0.gif'
+                    'filename' : 'p0e3.svg'
                   },
                   {
                     'name' : 'Salonen',
                     'id' : 3,
-                    'filname' : 'e0.gif'
+                    'filename' : 'p0e4.svg'
                   },
                   {
                     'name' : 'Intimscene',
                     'id' : 4,
-                    'filname' : 'e0.gif'
+                    'filename' : 'p0e5.svg'
                   },
                   {
                     'name' : 'Toiletter',
                     'id' : 5,
-                    'filname' : 'e0.gif'
+                    'filename' : 'p0e6.svg'
                   },
                   {
                     'name' : 'Elevator',
                     'id' : 6,
-                    'filname' : 'e0.gif'
+                    'filename' : 'p0e7.svg'
                   },
 ]
   },
   {
     'name' : '1. sal',
-    'filename' : 'p0.gif',
+    'filename' : 'p1.svg',
     'keywords' : [
                   {
                     'name' : 'Café',
                     'id' : 7,
-                    'filname' : 'e0.gif'
+                    'filename' : 'p1e4.svg'
                   },
                   {
                     'name' : 'Receptionen',
                     'id' : 8,
-                    'filname' : 'e0.gif'
+                    'filename' : 'p1e5.svg'
                   },
                   {
                     'name' : 'Toiletter',
                     'id' : 5,
-                    'filname' : 'e0.gif'
+                    'filename' : 'p1e6.svg'
                   },
                   {
                     'name' : 'Elevator',
                     'id' : 6,
-                    'filname' : 'e0.gif'
+                    'filename' : 'p1e7.svg'
                   },
 ]
   },
   {
     'name' : '2. sal',
-    'filename' : 'p0.gif',
+    'filename' : 'p2.svg',
     'keywords' : [
                   {
                     'name' : 'Toiletter',
                     'id' : 5,
-                    'filname' : 'e0.gif'
+                    'filename' : 'p2e2.svg'
                   },
                   {
                     'name' : 'Elevator',
                     'id' : 6,
-                    'filname' : 'e0.gif'
+                    'filename' : 'p2e3.svg'
                   },
                   {
                     'name' : 'Borgerservice',
                     'id' : 9,
-                    'filname' : 'e0.gif'
+                    'filename' : 'p2e4.svg'
                   },
 ]
   }
   , {
     'name' : '3. sal',
-    'filename' : 'p0.gif',
+    'filename' : 'p3.svg',
     'keywords' : [
                   {
                     'name' : 'Café',
                     'id' : 10,
-                    'filname' : 'e0.gif'
+                    'filename' : 'e0.svg'
                   },
                   {
                     'name' : 'Receptionen',
                     'id' : 11,
-                    'filname' : 'e0.gif'
+                    'filename' : 'e0.svg'
                   },
                   {
                     'name' : 'Borgerservice',
                     'id' : 4,
-                    'filname' : 'e0.gif'
+                    'filename' : 'e0.svg'
                   },
                   {
                     'name' : 'Salonen',
                     'id' : 3,
-                    'filname' : 'e0.gif'
+                    'filename' : 'e0.svg'
                   },
                   ]
   }
   , {
     'name' : '4. sal',
-    'filename' : 'p0.gif',
+    'filename' : 'p4.svg',
     'keywords' : [
                   {
                     'name' : 'Café',
                     'id' : 6,
-                    'filname' : 'e0.gif'
+                    'filename' : 'e0.svg'
                   },
                   {
                     'name' : 'Receptionen',
                     'id' : 12,
-                    'filname' : 'e0.gif'
+                    'filename' : 'e0.svg'
                   },
   ]
   }
