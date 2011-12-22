076d0dc75acbd923d129172607916a652e85c464
Troels Lenda <troelslenda@gmail.com>
2011-12-22 15:34:22 +0100

    revising techinal implementation

--- theme/setup.js
+++ theme/setup.js
@@ -1,31 +1,61 @@
 jQuery(function(){
-  console.log(data);
-
-
-  // append list
-
 
 
   // build floors list
-
   $(data).each(function(i){
-    $('.plans').append('<li>'+this.name+'</li>');
-  });
 
+    $('.floornav').append('<li>'+this.name+'</li>');
+    $('.keywords').append('<li><ul></ul></li>');
 
-
-  $('.plans li').click(function(){
-    setKeywords($('.plans li').index(this));
-
-  });
-
-
-  function setKeywords(index){
-    $('.keywords').html('');
-    $(data[index].keywords).each(function(i){
-      console.log(this);
-      $('.keywords').append('<li>'+this.name+'</li>');
+    $(this.keywords).each(function(i){
+      $('.keywords li:last-child ul').append('<li data-id="'+this.id+'">'+this.name+'</li>');
     });
+
+  });
+
+  // change overlay
+  $('.keywords li ul li').click(function(){
+    var clickedElementIndex = $('.keywords li ul li').index(this);
+    var indexa = 0;
+    var found = false;
+    $(data).each(function(fi){
+      $(this.keywords).each(function(ei){
+        if(indexa == clickedElementIndex) {
+          found = true;
+          if(found){
+            changeOverlay(fi,ei)
+            return false;
+          }
+        }
+        indexa++;
+      });
+      if(found){
+        return false;
+      }
+    });
+  });
+
+  $('.floornav li').click(function(){
+    var clickedFloorIndex = $('.floornav li').index(this);
+
+
+    changeFloorPlan(clickedFloorIndex);
+
+  });
+
+  function changeFloorPlan(index){
+    $('.floorplan-image').attr('src', 'files/' + data[index].filename);
+    $('.keywords li ul').hide();
+    $('.keywords li ul:eq('+index+')').show();
+    $('.floornav li:eq('+index+')').siblings().removeClass('red-gradient');
+    $('.floornav li:eq('+index+')').addClass('red-gradient');
   }
+
+  function changeOverlay(floorIndex,elementIndex) {
+    $('.element-image').attr('src', 'files/' + data[floorIndex].keywords[elementIndex].filename);
+  }
+
+
+  changeFloorPlan(0);
 
 });
