ba2e8992e275a8ab7fead50a22892b44a9e9493d
Troels Lenda <troelslenda@gmail.com>
2011-12-30 12:08:46 +0100

    cleanup in datafile

--- theme/data.js
+++ theme/data.js
@@ -2,307 +2,254 @@
  * @file
  * This data contains the data that should be provided by the service
  **/
+var data = [{
+  'name': 'Stuen',
+  'filename': 'p0.svg',
+  'keywords': [{
+    'name': 'Café',
+    'id': 1,
+    'filename': 'p0e1.svg'
+  }, {
+    'name': 'Receptionen',
+    'id': 2,
+    'filename': 'p0e2.svg'
+  }, {
+    'name': 'Borgerservice',
+    'id': 3,
+    'filename': 'p0e3.svg'
+  }, {
+    'name': 'Salonen',
+    'id': 4,
+    'filename': 'p0e4.svg'
+  }, {
+    'name': 'Intimscene',
+    'id': 5,
+    'filename': 'p0e5.svg'
+  }, {
+    'name': 'Hent og Bring',
+    'id': 6,
+    'filename': 'p0e6.svg'
+  }, {
+    'name': 'Aviser',
+    'id': 7,
+    'filename': 'p0e7.svg'
+  }, {
+    'name': 'Toiletter',
+    'id': 8,
+    'filename': 'p0e8.svg'
+  }, {
+    'name': 'Kopimaskine',
+    'id': 9,
+    'filename': 'p0e9.svg'
+  }, {
+    'name': 'Elevator',
+    'id': 10,
+    'filename': 'p0e10.svg'
+  },
 
-var data = [
-  {
-    'name' : 'Stuen',
-    'filename' : 'p0.svg',
-    'keywords' : [
-{
-  'name' : 'Café',
-  'id' : 0,
-  'filename' : 'p0e1.svg'
-},
-{
-  'name' : 'Receptionen',
-  'id' : 1,
-  'filename' : 'p0e2.svg'
-},
-{
-  'name' : 'Borgerservice',
-  'id' : 2,
-  'filename' : 'p0e3.svg'
-},
-{
-  'name' : 'Salonen',
-  'id' : 3,
-  'filename' : 'p0e4.svg'
-},
-{
-  'name' : 'Intimscene',
-  'id' : 4,
-  'filename' : 'p0e5.svg'
-},
-{
-  'name' : 'Toiletter',
-  'id' : 5,
-  'filename' : 'p0e6.svg'
-},
-{
-  'name' : 'Elevator',
-  'id' : 6,
-  'filename' : 'p0e7.svg'
-},{
-  'name' : 'Café',
-  'id' : 0,
-  'filename' : 'p0e1.svg'
-},
-{
-  'name' : 'Receptionen',
-  'id' : 1,
-  'filename' : 'p0e2.svg'
-},
-{
-  'name' : 'Borgerservice',
-  'id' : 2,
-  'filename' : 'p0e3.svg'
-},
-{
-  'name' : 'Salonen',
-  'id' : 3,
-  'filename' : 'p0e4.svg'
-},
-{
-  'name' : 'Intimscene',
-  'id' : 4,
-  'filename' : 'p0e5.svg'
-},
-{
-  'name' : 'Toiletter',
-  'id' : 5,
-  'filename' : 'p0e6.svg'
-},
-{
-  'name' : 'Elevator',
-  'id' : 6,
-  'filename' : 'p0e7.svg'
-},{
-  'name' : 'Café',
-  'id' : 0,
-  'filename' : 'p0e1.svg'
-},
-{
-  'name' : 'Receptionen',
-  'id' : 1,
-  'filename' : 'p0e2.svg'
-},
-{
-  'name' : 'Borgerservice',
-  'id' : 2,
-  'filename' : 'p0e3.svg'
-},
-{
-  'name' : 'Salonen',
-  'id' : 3,
-  'filename' : 'p0e4.svg'
-},
-{
-  'name' : 'Intimscene',
-  'id' : 4,
-  'filename' : 'p0e5.svg'
-},
-{
-  'name' : 'Toiletter',
-  'id' : 5,
-  'filename' : 'p0e6.svg'
-},
-{
-  'name' : 'Elevator',
-  'id' : 6,
-  'filename' : 'p0e7.svg'
-},{
-  'name' : 'Café',
-  'id' : 0,
-  'filename' : 'p0e1.svg'
-},
-{
-  'name' : 'Receptionen',
-  'id' : 1,
-  'filename' : 'p0e2.svg'
-},
-{
-  'name' : 'Borgerservice',
-  'id' : 2,
-  'filename' : 'p0e3.svg'
-},
-{
-  'name' : 'Salonen',
-  'id' : 3,
-  'filename' : 'p0e4.svg'
-},
-{
-  'name' : 'Intimscene',
-  'id' : 4,
-  'filename' : 'p0e5.svg'
-},
-{
-  'name' : 'Toiletter',
-  'id' : 5,
-  'filename' : 'p0e6.svg'
-},
-{
-  'name' : 'Elevator',
-  'id' : 6,
-  'filename' : 'p0e7.svg'
-},{
-  'name' : 'Café',
-  'id' : 0,
-  'filename' : 'p0e1.svg'
-},
-{
-  'name' : 'Receptionen',
-  'id' : 1,
-  'filename' : 'p0e2.svg'
-},
-{
-  'name' : 'Borgerservice',
-  'id' : 2,
-  'filename' : 'p0e3.svg'
-},
-{
-  'name' : 'Salonen',
-  'id' : 3,
-  'filename' : 'p0e4.svg'
-},
-{
-  'name' : 'Intimscene',
-  'id' : 4,
-  'filename' : 'p0e5.svg'
-},
-{
-  'name' : 'Toiletter',
-  'id' : 5,
-  'filename' : 'p0e6.svg'
-},
-{
-  'name' : 'Elevator',
-  'id' : 6,
-  'filename' : 'p0e7.svg'
-},{
-  'name' : 'Café',
-  'id' : 0,
-  'filename' : 'p0e1.svg'
-},
-{
-  'name' : 'Receptionen',
-  'id' : 1,
-  'filename' : 'p0e2.svg'
-},
-{
-  'name' : 'Borgerservice',
-  'id' : 2,
-  'filename' : 'p0e3.svg'
-},
-{
-  'name' : 'Salonen',
-  'id' : 3,
-  'filename' : 'p0e4.svg'
-},
-{
-  'name' : 'Intimscene',
-  'id' : 4,
-  'filename' : 'p0e5.svg'
-},
-{
-  'name' : 'Toiletter',
-  'id' : 5,
-  'filename' : 'p0e6.svg'
-},
-{
-  'name' : 'Elevator',
-  'id' : 6,
-  'filename' : 'p0e7.svg'
-},
-]
-  },
-  {
-    'name' : '1. sal',
-    'filename' : 'p1.svg',
-    'keywords' : [
-                  {
-                    'name' : 'Café',
-                    'id' : 7,
-                    'filename' : 'p1e4.svg'
-                  },
-                  {
-                    'name' : 'Receptionen',
-                    'id' : 8,
-                    'filename' : 'p1e5.svg'
-                  },
-                  {
-                    'name' : 'Toiletter',
-                    'id' : 5,
-                    'filename' : 'p1e6.svg'
-                  },
-                  {
-                    'name' : 'Elevator',
-                    'id' : 6,
-                    'filename' : 'p1e7.svg'
-                  },
-]
-  },
-  {
-    'name' : '2. sal',
-    'filename' : 'p2.svg',
-    'keywords' : [
-                  {
-                    'name' : 'Toiletter',
-                    'id' : 5,
-                    'filename' : 'p2e2.svg'
-                  },
-                  {
-                    'name' : 'Elevator',
-                    'id' : 6,
-                    'filename' : 'p2e3.svg'
-                  },
-                  {
-                    'name' : 'Borgerservice',
-                    'id' : 9,
-                    'filename' : 'p2e4.svg'
-                  },
-]
-  }
-  , {
-    'name' : '3. sal',
-    'filename' : 'p3.svg',
-    'keywords' : [
-                  {
-                    'name' : 'Café',
-                    'id' : 10,
-                    'filename' : 'e0.svg'
-                  },
-                  {
-                    'name' : 'Receptionen',
-                    'id' : 11,
-                    'filename' : 'e0.svg'
-                  },
-                  {
-                    'name' : 'Borgerservice',
-                    'id' : 4,
-                    'filename' : 'e0.svg'
-                  },
-                  {
-                    'name' : 'Salonen',
-                    'id' : 3,
-                    'filename' : 'e0.svg'
-                  },
-                  ]
-  }
-  , {
-    'name' : '4. sal',
-    'filename' : 'p4.svg',
-    'keywords' : [
-                  {
-                    'name' : 'Café',
-                    'id' : 6,
-                    'filename' : 'e0.svg'
-                  },
-                  {
-                    'name' : 'Receptionen',
-                    'id' : 12,
-                    'filename' : 'e0.svg'
-                  },
   ]
-  }
+}, {
+  'name': '1. sal',
+  'filename': 'p1.gif',
+  'keywords': [{
+    'name': 'Kunsthistorie',
+    'id': 11,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Digte',
+    'id': 12,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Skuespil',
+    'id': 13,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Literaturhistorie',
+    'id': 14,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Sprog',
+    'id': 15,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Biografier',
+    'id': 16,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Skønlitteratur (Udland)',
+    'id': 17,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Skønlitteratur (DK)',
+    'id': 18,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Krimier',
+    'id': 19,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Toiletter',
+    'id': 8,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Elevator',
+    'id': 10,
+    'filename': 'noimg.png'
+  }, ]
+}, {
+  'name': '2. sal',
+  'filename': 'p2.gif',
+  'keywords': [{
+    'name': 'Børn og unge',
+    'id': 20,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Musikbøger',
+    'id': 21,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Noder',
+    'id': 22,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Lytteposter',
+    'id': 23,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'CD\'ere',
+    'id': 24,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Tegneserier',
+    'id': 25,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Undervisningslokale',
+    'id': 26,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Instrumentalzone',
+    'id': 27,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Tidsskrifter',
+    'id': 28,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Toiletter',
+    'id': 8,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Elevator',
+    'id': 10,
+    'filename': 'noimg.png'
+  }, ]
+}, {
+  'name': '3. sal',
+  'filename': 'p3.gif',
+  'keywords': [{
+    'name': 'EU',
+    'id': 29,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Kogebøger',
+    'id': 30,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Natur og Teknik',
+    'id': 31,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Sundhed',
+    'id': 32,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Rejser',
+    'id': 33,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Politik og økonomi',
+    'id': 34,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Samfund',
+    'id': 35,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Sociologi',
+    'id': 36,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'IT-bøger',
+    'id': 37,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Religion',
+    'id': 38,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Psykologi',
+    'id': 39,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Filosofi',
+    'id': 40,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Elevator',
+    'id': 10,
+    'filename': 'noimg.png'
+  },
+
+  ]
+}, {
+  'name': '4. sal',
+  'filename': 'p4.gif',
+  'keywords': [{
+    'name': 'Webværksted',
+    'id': 41,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Kopimaskine',
+    'id': 9,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Tidskrifter',
+    'id': 43,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Sport og Idræt',
+    'id': 44,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Historie',
+    'id': 45,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Biografier',
+    'id': 16,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Håndarbejde',
+    'id': 47,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Vejvisere',
+    'id': 48,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Kraks blå bog',
+    'id': 49,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Toiletter',
+    'id': 8,
+    'filename': 'noimg.png'
+  }, {
+    'name': 'Elevator',
+    'id': 10,
+    'filename': 'noimg.png'
+  }, ]
+}
 
 ];
