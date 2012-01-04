464f3a513b560af09bb7d68ab17a41d62a222c82
Troels Lenda <troelslenda@gmail.com>
2012-01-04 13:09:08 +0100

    adding new overlay files to project

--- data.js
+++ data.js
@@ -50,39 +50,39 @@ var data = [{
   ]
 }, {
   'name': '1. sal',
-  'filename': 'p1.gif',
+  'filename': 'p1.svg',
   'keywords': [{
     'name': 'Kunsthistorie',
     'id': 11,
-    'filename': 'noimg.png'
+    'filename': 'P1e1_kunst.svg'
   }, {
     'name': 'Digte',
     'id': 12,
-    'filename': 'noimg.png'
+    'filename': 'P1e4_digte.svg'
   }, {
     'name': 'Skuespil',
     'id': 13,
-    'filename': 'noimg.png'
+    'filename': 'P1e4_digte.svg'
   }, {
     'name': 'Literaturhistorie',
     'id': 14,
-    'filename': 'noimg.png'
+    'filename': 'P1e1_kunst.svg'
   }, {
     'name': 'Sprog',
     'id': 15,
-    'filename': 'noimg.png'
+    'filename': 'P1e5_sprog.svg'
   }, {
     'name': 'Biografier',
     'id': 16,
-    'filename': 'noimg.png'
+    'filename': 'P1e6_biografier.svg'
   }, {
     'name': 'Skønlitteratur (Udland)',
     'id': 17,
-    'filename': 'noimg.png'
+    'filename': 'P1e7_skonUD.svg'
   }, {
     'name': 'Skønlitteratur (DK)',
     'id': 18,
-    'filename': 'noimg.png'
+    'filename': 'P1e8_skonDK.svg'
   }, {
     'name': 'Krimier',
     'id': 19,
@@ -90,7 +90,7 @@ var data = [{
   }, {
     'name': 'Toiletter',
     'id': 8,
-    'filename': 'noimg.png'
+    'filename': 'P1e9_toiletter.svg'
   }, {
     'name': 'Elevator',
     'id': 10,
@@ -98,53 +98,53 @@ var data = [{
   }, ]
 }, {
   'name': '2. sal',
-  'filename': 'p2.gif',
+  'filename': 'P2.svg',
   'keywords': [{
     'name': 'Børn og unge',
     'id': 20,
-    'filename': 'noimg.png'
+    'filename': 'P2e1.svg'
   }, {
     'name': 'Musikbøger',
     'id': 21,
-    'filename': 'noimg.png'
+    'filename': 'P2e2.svg'
   }, {
     'name': 'Noder',
     'id': 22,
-    'filename': 'noimg.png'
+    'filename': 'P2e3.svg'
   }, {
     'name': 'Lytteposter',
     'id': 23,
-    'filename': 'noimg.png'
+    'filename': 'P2e4.svg'
   }, {
     'name': 'CD\'ere',
     'id': 24,
-    'filename': 'noimg.png'
+    'filename': 'P2e5.svg'
   }, {
     'name': 'Tegneserier',
     'id': 25,
-    'filename': 'noimg.png'
+    'filename': 'P2e6.svg'
   }, {
     'name': 'Undervisningslokale',
     'id': 26,
-    'filename': 'noimg.png'
+    'filename': 'P2e7.svg'
   }, {
     'name': 'Instrumentalzone',
     'id': 27,
-    'filename': 'noimg.png'
+    'filename': 'P2e8.svg'
   }, {
     'name': 'Tidsskrifter',
     'id': 28,
-    'filename': 'noimg.png'
+    'filename': 'P2e9.svg'
   }, {
     'name': 'Toiletter',
     'id': 8,
-    'filename': 'noimg.png'
+    'filename': 'P2e10.svg'
   }, {
     'name': 'Elevator',
     'id': 10,
     'filename': 'p0e10.svg'
   }, ]
-}, {
+},/* {
   'name': '3. sal',
   'filename': 'p3.gif',
   'keywords': [{
@@ -202,49 +202,49 @@ var data = [{
   },
 
   ]
-}, {
+},*/ {
   'name': '4. sal',
-  'filename': 'p4.gif',
+  'filename': 'p4.svg',
   'keywords': [{
     'name': 'Webværksted',
     'id': 41,
-    'filename': 'noimg.png'
+    'filename': 'P4e1.svg'
   }, {
     'name': 'Kopimaskine',
     'id': 9,
-    'filename': 'noimg.png'
+    'filename': 'P4e2.svg'
   }, {
     'name': 'Tidskrifter',
     'id': 43,
-    'filename': 'noimg.png'
+    'filename': 'P4e3.svg'
   }, {
     'name': 'Sport og Idræt',
     'id': 44,
-    'filename': 'noimg.png'
+    'filename': 'P4e4.svg'
   }, {
     'name': 'Historie',
     'id': 45,
-    'filename': 'noimg.png'
+    'filename': 'P4e5.svg'
   }, {
     'name': 'Biografier',
     'id': 16,
-    'filename': 'noimg.png'
+    'filename': 'P4e6.svg'
   }, {
     'name': 'Håndarbejde',
     'id': 47,
-    'filename': 'noimg.png'
+    'filename': 'P4e7.svg'
   }, {
     'name': 'Vejvisere',
     'id': 48,
-    'filename': 'noimg.png'
+    'filename': 'P4e8.svg'
   }, {
     'name': 'Kraks blå bog',
     'id': 49,
-    'filename': 'noimg.png'
+    'filename': 'P4e9.svg'
   }, {
     'name': 'Toiletter',
     'id': 8,
-    'filename': 'noimg.png'
+    'filename': 'P4e10.svg'
   }, {
     'name': 'Elevator',
     'id': 10,
